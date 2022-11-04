bb5b22c11edc29e76962b2eeb12781c615e8e096
Timofey Bystrov <timapro21@gmail.com>
2022-11-04 09:55:17 +0300

    Tmp

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,32 +1,60 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
-
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = WindowState(
+            placement = WindowPlacement.Floating,
+            size = DpSize(640.dp, 480.dp),
+            position = WindowPosition.Aligned(Alignment.Center)
+        )
+    ) {
+        MaterialTheme(
+            content = {
+                Column(
+                    content = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.End,
+                            content = {
+                                Text("Skip")
+                            })
+                        Text("15:00")
+                        Text("Timer layout")
+                        Button(onClick = {}, content = { Text("Start/Stop button") })
+                        Row(content = {
+                            Column(content = {
+                                Text("Round")
+                                Text("2/4")
+                            })
+                            Column(content = {
+                                Text("Goal")
+                                Text("1/4")
+                            })
+                        })
+                    })
+            }
+        )
     }
 }
