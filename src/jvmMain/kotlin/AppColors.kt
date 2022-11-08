fc8b361e073d47c2b53232070d4f6bdc89b242dc
timtheemployee <timapro21@gmail.com>
2022-11-08 12:42:34 +0300

    Updates

---
+++ src/jvmMain/kotlin/AppColors.kt
@@ -0,0 +1,8 @@
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val blue = Color(red = 48, green = 57, blue = 95)
+    val red = Color(red = 209, green = 110, blue = 116)
+    val white = Color(red = 255, green = 255, blue = 255)
+    val textColor = Color(red = 237, green = 232, blue = 218)
+}
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,14 +1,27 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,56 +44,106 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         state = WindowState(
             placement = WindowPlacement.Floating,
-            size = DpSize(640.dp, 480.dp),
+            size = DpSize(320.dp, 480.dp),
             position = WindowPosition.Aligned(Alignment.Center)
         )
     ) {
         MaterialTheme(
             content = {
                 Column(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().background(AppColors.red),
                     content = {
-                        IconButton(
-                            modifier = Modifier.align(Alignment.End),
-                            onClick = {
+                        toolbar(
+                            onSkip = timerViewModel::onSkipClicked,
+                            onClose = {
                                 timerViewModel.onCloseClicked()
                                 exitApplication()
-                            },
-                            content = {
-                                Icon(
-                                    Icons.Default.Close,
-                                    contentDescription = null,
-                                    tint = MaterialTheme.colors.primary
-                                )
                             }
-                        )
-                        TextButton(
-                            modifier = Modifier.align(Alignment.End),
-                            content = { Text("Skip") },
-                            onClick = timerViewModel::onSkipClicked,
                         )
                         Text(
                             modifier = Modifier.align(Alignment.CenterHorizontally),
-                            text = timer.value
+                            text = timer.value,
+                            style = MaterialTheme.typography.h2,
+                            color = AppColors.textColor
                         )
-                        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "Timer layout")
+                        Text(
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            text = "Timer layout",
+                            color = AppColors.textColor
+                        )
                         Button(
                             modifier = Modifier.align(Alignment.CenterHorizontally),
                             onClick = timerViewModel::onActionClicked,
-                            content = { Text("Start/Stop button") })
-
-                        Row(modifier = Modifier.align(Alignment.CenterHorizontally), content = {
-                            Column(content = {
-                                Text("Round")
-                                Text("2/4")
-                            })
-                            Column(content = {
-                                Text("Goal")
-                                Text("1/4")
-                            })
-                        })
+                            content = { Text("Start/Stop button", color = AppColors.textColor) })
+                        footer()
                     })
             }
         )
     }
+}
+
+
+@Composable
+private fun footer() {
+    Box(modifier = Modifier.fillMaxSize(), content = {
+        Card(
+            shape = RoundedCornerShape(topStartPercent = 10, topEndPercent = 10),
+            modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter),
+            content = {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally, content = {
+                                Text("GOALS", color = AppColors.blue, style = MaterialTheme.typography.subtitle1)
+                                Spacer(Modifier.padding(vertical = 8.dp))
+                                Text("2/4", color = AppColors.blue, style = MaterialTheme.typography.h4)
+                            })
+                        Divider(
+                            modifier = Modifier.width(2.dp).fillMaxHeight(0.3f).padding(vertical = 2.dp),
+                            color = AppColors.blue,
+                        )
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            content = {
+                                Text("ROUNDS", color = AppColors.blue, style = MaterialTheme.typography.subtitle1)
+                                Spacer(Modifier.padding(vertical = 8.dp))
+                                Text("4", color = AppColors.blue, style = MaterialTheme.typography.h4)
+                            })
+                    })
+            })
+    })
+}
+
+@Composable
+private fun ColumnScope.toolbar(
+    onSkip: () -> Unit,
+    onClose: () -> Unit
+) {
+    Row(
+        modifier = Modifier.align(Alignment.End),
+        content = {
+            IconButton(
+                content = {
+                    Icon(
+                        Icons.Default.Refresh,
+                        contentDescription = null,
+                        tint = AppColors.textColor
+                    )
+                },
+                onClick = onSkip,
+            )
+            IconButton(
+                onClick = onClose,
+                content = {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = null,
+                        tint = AppColors.textColor
+                    )
+                }
+            )
+        }
+    )
 }
